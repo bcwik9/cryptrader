4bf0d09ca7ffc9df9a176d2710ebd34c9d88a406
bcwik <bcwik9@gmail.com>
2018-01-18 06:55:40 +0000

    display price of bitcoin in gdax

---
+++ app/assets/javascripts/gdax.js
@@ -0,0 +1,46 @@
+Gdax = function() {
+
+  var btc_price_placeholder = $("#btc-price")
+
+  var init = function() {
+    console.log("attempting to connect to Gdax...")
+    if ("WebSocket" in window){
+      console.log("WebSocket is supported");
+      var ws = new WebSocket("wss://ws-feed.gdax.com");
+
+      // see https://docs.gdax.com/?ruby#subscribe
+      var options = {
+          "type": "subscribe",
+          "product_ids": [
+              "BTC-USD"
+          ],
+          "channels": [
+              "level2",
+              "heartbeat",
+              {
+                  "name": "ticker",
+                  "product_ids": [
+                      "BTC-USD"
+                  ]
+              }
+          ]
+      }
+
+      ws.onopen = function() {
+        console.log("websocket opened. sending message")
+        ws.send(JSON.stringify(options))
+      }
+
+      ws.onmessage = function (event){ 
+        var msg = event.data;
+        console.log("Message is received: ", msg);
+        data = JSON.parse(event.data)
+        if(data['type'] == 'ticker'){
+          btc_price_placeholder.text("$" + data['price'])
+        }
+      };
+    }
+  }
+
+  init()
+}
