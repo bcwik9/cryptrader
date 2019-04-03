62eca7ab7719ef9fa23a16fc44785d46e8f7edec
bcwik <bcwik9@gmail.com>
2019-04-03 01:03:18 +0000

    remove rails

--- source/javascripts/coinbase.js
+++ source/javascripts/coinbase.js
@@ -3,7 +3,7 @@ Coinbase = function() {
   var btc_price_placeholder = $("#btc-price")
 
   var init = function() {
-    console.log("attempting to connect to Gdax...")
+    console.log("attempting to connect to Coinbase...")
     if ("WebSocket" in window){
       console.log("WebSocket is supported");
       var ws = new WebSocket("wss://ws-feed-public.sandbox.pro.coinbase.com");
@@ -48,7 +48,7 @@ Coinbase = function() {
 
       ws.onclose = function(){ 
         // websocket is closed.
-        console.log("Connection to gdax closed..."); 
+        console.log("Connection to Coinbase closed..."); 
       };
         
       window.onbeforeunload = function(event) {
